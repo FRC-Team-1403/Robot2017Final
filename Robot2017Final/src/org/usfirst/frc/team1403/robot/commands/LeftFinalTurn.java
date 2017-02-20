072f2ac51ce7a1d8603e820fb3b8d8d68d40d996
jshah1006 <jshah1006@gmail.com>
2017-02-20 13:43:28 -0500

    Vision test 1
    First commit with vision for 2017 bot

---
+++ Robot2017Final/src/org/usfirst/frc/team1403/robot/commands/LeftFinalTurn.java
@@ -0,0 +1,51 @@
+package org.usfirst.frc.team1403.robot.commands;
+
+import org.usfirst.frc.team1403.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class LeftFinalTurn extends Command {
+
+    public LeftFinalTurn() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.neededAngle = Robot.turn;
+    	Robot.currentAngle = Robot.angle;
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	System.out.println("Gyro angle " + Robot.angle);
+    	
+    	if (Robot.angle > 0){
+    	System.out.println("Robot is Moving");
+    	Robot.driveTrain.motor4.set(-0.2);
+    	Robot.driveTrain.motor5.set(-0.2);
+    	Robot.driveTrain.motor6.set(-0.2);
+    	Robot.driveTrain.motor7.set(-0.2);
+    	}   	
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Robot.angle <= 0;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrain.motor4.set(0);
+    	Robot.driveTrain.motor5.set(0);
+    	Robot.driveTrain.motor6.set(0);
+    	Robot.driveTrain.motor7.set(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ Robot2017Final/src/org/usfirst/frc/team1403/robot/commands/VisionAimAssist.java
@@ -0,0 +1,68 @@
+package org.usfirst.frc.team1403.robot.commands;
+
+import org.usfirst.frc.team1403.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class VisionAimAssist extends Command {
+	
+	private double diff;
+	
+    public VisionAimAssist() {
+    
+        requires(Robot.driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+
+    	diff = SmartDashboard.getNumber("difference", 321);
+    	SmartDashboard.putBoolean("Aligned", false);
+    	
+    	if (diff <= -68) {
+    		
+    		
+    		Robot.driveTrain.motor4.set(0.3);
+    		Robot.driveTrain.motor5.set(0.3);
+    		Robot.driveTrain.motor6.set(0.3);
+    		Robot.driveTrain.motor7.set(0.3);
+    }
+    		    	
+       	if (diff >= -75){
+    		    		
+       		
+        	Robot.driveTrain.motor4.set(-0.3);
+        	Robot.driveTrain.motor5.set(-0.3);
+        	Robot.driveTrain.motor6.set(-0.3);
+        	Robot.driveTrain.motor7.set(-0.3);
+        	
+        	
+       	}
+    		    	
+
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return diff < -68 && diff > -75;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrain.motor4.set(0);
+    	Robot.driveTrain.motor5.set(0);
+    	Robot.driveTrain.motor6.set(0);
+    	Robot.driveTrain.motor7.set(0);
+    	SmartDashboard.putBoolean("Aligned", true);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
